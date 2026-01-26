68a419efb596d3add8e89748c3323fa59cfe375c
m1ss1ngk01 <151236566+m1ss1ngk01@users.noreply.github.com>
2026-01-26 16:08:31 +0000

    movement or sumn

--- GGJ 2026/Assets/Player.cs
+++ GGJ 2026/Assets/Player.cs
@@ -3,13 +3,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    #region [ Unserialised Fields ]
-    [SerializeField] private float _forceMultiplied;
-    #endregion
-    
-    #region [ Unserialised Fields ]
+    [SerializeField] private float _walkspeed;
+    [SerializeField] private float _runspeed;
+
     private Rigidbody2D _rigidbody;
-    #endregion
+
     
     
     
@@ -18,40 +16,55 @@ public class Player : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-
     public void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 movementVector = new Vector2(horizontal, vertical);
+        Vector2 normalizedMovementVector = Vector2.ClampMagnitude(movementVector, 1);
+        Vector2 force = normalizedMovementVector * _walkspeed;
+        if (Input.GetButton("Sprint"))
+        {
+            force = normalizedMovementVector * _runspeed;
+        }
+        _rigidbody.linearVelocity = force;
+    }
+
+
+    public void UpdateOld()
     {
         Vector2 movementVector = Vector2.zero;
         bool moving = false;
         
         if (Input.GetKey(KeyCode.W))
         {
-            movementVector += new Vector2(0, 1);
+            movementVector += new Vector2(0, 0.5f);
             moving = true;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            movementVector += new Vector2(0, -1);
+            movementVector += new Vector2(0, -0.5f);
             moving = true;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            movementVector += new Vector2(1, 0);
+            movementVector += new Vector2(0.5f, 0);
             moving = true;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            movementVector += new Vector2(-1, 0);
+            movementVector += new Vector2(-0.5f, 0);
             moving = true;
         }
 
         if (moving)
         {
             Vector2 normalizedMovementVector = movementVector.normalized;
-            Vector2 force = normalizedMovementVector * _forceMultiplied;
+            Vector2 force = normalizedMovementVector * _walkspeed;
             
             _rigidbody.AddForce(force);
         }
